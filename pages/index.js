9cb873ccf29233c98565b3375924b9dd0ba7f911
Fizioh <batrom@laposte.net>
2023-04-26 14:35:44 +0200

    feat integrate with Chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
     return(
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I'm a full-stack developer based in Paris, France.
+            </Box>
+            
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Yassine Benosmane
+                    </Heading>
+                    <p>Digital Autodidact (Developer / Blogger / Mentor)</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
